cc28b9349d607a4a011f085c1947b326be892892
kenny-tech <kenny4real2001@gmail.com>
2021-02-18 21:15:35 +0100

    Created signin endpoint

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/signup', 'Api\AuthController@signup');
+Route::post('/signin', 'Api\AuthController@signin');
